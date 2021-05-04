daacf4b3120a63ff0de3a962b9e98db9859d8e8e
Arik Alexandrov <arikxl@gmail.com>
2021-05-04 13:53:19 +0300

    BEER

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
 import { BookApp } from './pages/BookApp.jsx'
-import { MailApp } from 'apps/Mail/MailApp.jsx';
-console.log('MailApp:', MailApp)
+import { MailApp } from './apps/Mail/MailApp.jsx';
 
 export function App() {
     return (
         <section className="app">
             {/* <BookApp /> */}
+            <MailApp />
         </section>
     )
 }
