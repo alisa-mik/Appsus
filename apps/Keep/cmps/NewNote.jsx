5a6a431aa8f37c235b4ea57f4adc3aafa8072a76
Alisa <youremail@example.com>
2021-05-07 14:20:21 +0300

    update

--- apps/Keep/cmps/NewNote.jsx
+++ apps/Keep/cmps/NewNote.jsx
@@ -1,40 +1,46 @@
-export function NewNote({ note }) {
+import {noteService} from '../services/note-service.js'
 
-    switch(note.type) {
-        case "NoteText": {
-            return (
-                // <h4>hi I'm text</h4>
-                <NoteTxt note={note} />
-            )
+export class NewNote extends React.Component {
+    
+
+    state={
+        selectedBtn: 'Whats on your mind...',
+        txt: {
+            txt : 'Whats on your mind...',
+            img : 'Enter image url...',
+            vid : 'Enter video url...',
+            todo: 'Enter comma separated list...'
+
         }
-        case "NoteImg": {
-            return (
-                <NoteImg note={note} />
-            )
-        }
-        case "NoteVideo": {
-            return (
-                <NoteVideo note={note} />
-            )
-        }
-        default: {
-            return (
-              <h4>Hi i'm default</h4>
-            )
-          }
-        // case "NoteTodos": {
-        //     return (
-        //         <NoteTxt note={note} />
-        //     )
-        // }
-        // case "Notetxt": {
-        //     return (
-        //         <NoteTxt note={note} />
-        //     )
-        // }
-        // case "Notetxt": {
-        //     return (
-        //         <NoteTxt note={note} />
-        //     )
-        // }
     }
+    onChangeType = (ev) => {
+        console.log(ev.target.id)
+        this.setState({selectedBtn : this.state.txt[ev.target.id]})
+    }
+
+    handleKeyPress = (ev) => {
+        if(event.key === 'Enter'){
+          
+          noteService.addNote(ev.target.id, ev.target.value)
+          console.log("Aa")
+          this.setState({selectedBtn : this.state.txt['vid']})
+        }
+      }
+    
+    
+    render() {
+      return (
+           <div className="new-note">
+
+            <input id="new-note-input" type="text" placeholder={this.state.selectedBtn} onKeyPress={this.handleKeyPress}/>
+            <button id="txt" onClick={this.onChangeType}>txt</button>
+            <button id="img" onClick={this.onChangeType}>img</button>
+            <button id="vid" onClick={this.onChangeType}>Video</button>
+            <button id="todo" onClick={this.onChangeType}>todos</button>
+
+           </div>
+     )}
+      }
+
+
+  
